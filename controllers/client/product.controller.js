942f27bbe9439d1d222af74697c3ffad18463a17
3msinhvien <dotung.ptit@gmail.com>
2024-07-18 02:28:43 +0700

    Làm trang danh sách sản phẩm client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,18 @@
-module.exports.index = (req, res) => {
-    res.render("client/pages/products/index", {
-        pageTitle: "Danh sách sản phẩm"
-    });
-};
+const Product = require("../../models/product.model");
+
+// [GET] /products/
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    status: "active",
+    deleted: false
+  });
+
+  for (const item of products) {
+    item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+  }
+
+  res.render("client/pages/products/index", {
+    pageTitle: "Danh sách sản phẩm",
+    products: products
+  });
+}
